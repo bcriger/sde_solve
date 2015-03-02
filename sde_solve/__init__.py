b78de080e7b4fc622dbf478c75f07f2271ccdd83
Ben Criger <bcriger@gmail.com>
2015-03-02 09:43:49 +0100

    Changes to __init__.py, trying to fix weird import error

--- sde_solve/__init__.py
+++ sde_solve/__init__.py
@@ -1,3 +1,14 @@
 from sde_solve import *
 
 __all__ = ['sde_platen_15']
+
+__version__ = (0, 1, 0)
+
+import sde_solve
+
+__modules = [sde_solve]
+map(reload, __modules)
+
+from sde_solve import *
+
+__all__ = reduce(lambda a, b: a+b, map(lambda mod: mod.__all__, __modules)) + ['__version__']
